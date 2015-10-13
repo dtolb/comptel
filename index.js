b0d020d437c372a965b5df3987a165201f25a77b
Daniel Tolbert <dtolbert@bandwidth.com>
2015-10-13 16:35:40 -0400

    Multiple Numbers

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var xml = require('node-bandwidth').xml;
-var tn = require('./config.json').phoneNumber;
-var numCalls = 0;
-var callStart;
-var callEnd;
+var tns = require('./config.json');
+var cdr = {
+	tn1: {
+		numCalls: 0
+	},
+	tn2: {
+		numCalls: 0
+	},
+	tn3: {
+		numCalls: 0
+	}
+};
 
 /// Start the XML response
 var response = new xml.Response();
@@ -22,21 +30,46 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-//Add from number
 //three sets of each number
 app.get('/incomingCall', function(req, res) {
-	if(req.query && req.query.eventType && req.query.eventType === 'answer' &&
-		req.query.to === tn){
-			numCalls += 1;
-			io.emit('numCalls', numCalls);
-			callStart = new Date();
-			io.emit('caller', req.query.from);
-			res.send(bxml);
+	if(req.query && req.query.eventType && req.query.eventType === 'answer') {
+		res.send(bxml);
+		if(req.query.to === tns.tn1) {
+			cdr.tn1.numCalls += 1;
+			io.emit('numCalls1', cdr.tn1.numCalls);
+			cdr.tn1.callStart = new Date();
+			io.emit('caller1', req.query.from);
+		}
+		if(req.query.to === tns.tn2) {
+			cdr.tn2.numCalls += 1;
+			io.emit('numCalls2', cdr.tn2.numCalls);
+			cdr.tn2.callStart = new Date();
+			io.emit('caller2', req.query.from);
+		}
+		if(req.query.to === tns.tn3) {
+			cdr.tn3.numCalls += 1;
+			io.emit('numCalls3', cdr.tn3.numCalls);
+			cdr.tn3.callStart = new Date();
+			io.emit('caller3', req.query.from);
+		}
 	}
 	else if(req.query && req.query.eventType && req.query.eventType === 'hangup'){
-		callEnd = new Date();
-		var duration = (callEnd - callStart) / 1000;
-		io.emit('duration', duration);
+		if(req.query.to === tns.tn1) {
+			cdr.tn1.callEnd = new Date();
+			cdr.tn1.duration = (cdr.tn1.callEnd - cdr.tn1.callStart) / 1000;
+			io.emit('duration1', cdr.tn1.duration);
+
+		}
+		else if(req.query.to === tns.tn2) {
+			cdr.tn2.callEnd = new Date();
+			cdr.tn2.duration = (cdr.tn2.callEnd - cdr.tn2.callStart) / 1000;
+			io.emit('duration2', cdr.tn2.duration);
+		}
+		else if(req.query.to === tns.tn3) {
+			cdr.tn3.callEnd = new Date();
+			cdr.tn3.duration = (cdr.tn3.callEnd - cdr.tn3.callStart) / 1000;
+			io.emit('duration3', cdr.tn3.duration);
+		}
 	}
 	else {
 		res.send({status: 200});
